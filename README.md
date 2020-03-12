8d19b4a64d514556a0f6ec5a7b47ba11f6df204d
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-03-12 15:06:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# db-hack
+
+This project is a collection of three scripts that can edit entries in the database using CRUD operations. `fix_marks` looks for every bad mark for a certain student and replaces it with better ones, `remove_chastisements` deletes every chastisement for a student and `create_commendation` creates a random commendation for a selected student and subject.
+
+### How to install
+
+Python3 should be already installed. Also you should clone [e-diary](https://github.com/devmanorg/e-diary) repo to your computer.
+
+### Launch
+
+1. Copy `scripts.py` to the e-diary project directory.
+
+2. Open the interactive shell:
+
+```
+python3 manage.py shell
+```
+
+3. Import the scripts:
+
+```
+import scripts
+```
+
+4. Run the scripts in the shell:
+
+```
+scripts.fix_marks('Белозеров Авдей')
+scripts.remove_chastisements('Белозеров Авдей')
+scripts.create_commendation('Белозеров Авдей', 'Краеведение')
+```
+
+`fix_marks` and `remove_chastisements` require you to input a student's name, while `create_commendation` requires both student's name and the title of the subject. It is recommended to use full names, as the scripts will not work if they find more than one match for the name input.
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ scripts.py
@@ -0,0 +1,59 @@
+from datacenter.models import Schoolkid
+from datacenter.models import Mark
+from datacenter.models import Chastisement
+from datacenter.models import Lesson
+from datacenter.models import Commendation
+import random
+
+def fix_marks(name):
+    kid = Schoolkid.objects.get(full_name__contains=name)
+    bad_marks = Mark.objects.filter(schoolkid=kid, points__lte=3)
+    for mark in bad_marks:
+        mark.points = 5
+        mark.save()
+
+def remove_chastisements(name):
+    kid = Schoolkid.objects.get(full_name__contains=name)
+    chastisements = Chastisement.objects.filter(schoolkid=kid)
+    chastisements.delete()
+
+def create_commendation(name, subject):
+    commendations = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!',
+    ]
+    kid = Schoolkid.objects.get(full_name__contains=name)
+    lessons = Lesson.objects.filter(year_of_study=kid.year_of_study, group_letter=kid.group_letter, subject__title=subject)
+    lessons = lessons.order_by('-date')
+    latest_lesson = lessons[0]
+    commendation = Commendation.objects.create(text=random.choice(commendations), created=latest_lesson.date, schoolkid=kid,
+        subject=latest_lesson.subject, teacher=latest_lesson.teacher)
+    commendation.save()
